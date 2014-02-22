c4288e9ad3f19c728cf7f44c6d4e1fd6bc412a5c
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-02-23 00:34:27 +0200

    add migrations

---
+++ migrations/versions/506aa9090f00_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 506aa9090f00
+Revises: 466119528f1e
+Create Date: 2014-02-23 00:09:34.298217
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '506aa9090f00'
+down_revision = '466119528f1e'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
